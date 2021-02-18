36bb222c9d06a73ab971f4d8b54e4bace1d2616c
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2021-02-18 11:43:15 -0300

    creando config inicial para practica feign

---
+++ src/test/java/com/jmr/practica/feign_practica/feignpractica/FeignPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmr.practica.feign_practica.feignpractica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignPracticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
